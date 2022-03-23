b7b060486dfe0958373e1d32cb358fb0f5d676e8
Keaton Player <keatonplayer18@gmail.com>
2022-03-22 18:07:45 -0600

    Implemented cubed power and remainder made math symbols identical

--- calc.js
+++ calc.js
@@ -14,29 +14,30 @@ mathSymbol = tokens[0];
 num1 = Number(tokens[1]);
 num2 = Number(tokens[2]);
 
-if (mathSymbol === "+"){
+if (mathSymbol === "add"){
 	console.log(num1 + num2);
 }
-else if (mathSymbol === "*"){
+else if (mathSymbol === "times"){
 	console.log(num1 * num2);
 }
-else if (mathSymbol === "-"){
+else if (mathSymbol === "subtract"){
 	console.log(num1 - num2)
 }
-
-else if (mathSymbol === "/"){ 
+else if (mathSymbol === "divide"){ 
     console.log (num1 / num2)
 }
-
 else if (mathSymbol === "square"){
      console.log ( Math.sqrt(num1))
 }
-
-console.log('mathSymbol', mathSymbol);
-console.log('num1', num1);
-console.log('num2', num2);
-
+else if (mathSymbol === "cube"){
+     console.log (num1*num1*num1)
+}
+else if (mathSymbol === "power"){
+     console.log (Math.pow(num1, num2))
+} 
+else if (mathSymbol === "remainder"){
+     console.log(num1%num2)
+}
 	// This line closes the connection to the command line interface.
 	reader.close()
-
 });
